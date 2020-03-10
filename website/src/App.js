b6ee2b294134cebbb8d958eb866297410014acb9
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-03-10 10:59:49 +0530

    added content

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Index from './components/index.component';
+//import Greet from './components/Greet.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Bootstrap, Grid, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      {/* <Greet /> */}
+    </div>
+  );
+}
+
+export default App;
