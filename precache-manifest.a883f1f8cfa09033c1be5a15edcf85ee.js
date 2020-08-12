d1248e0e01fe14bb3d5a4246a2cad4617e7dc470
unknown <himanshu25031991@gmail.com>
2020-08-12 20:07:30 +0530

    Updates

---
+++ precache-manifest.a883f1f8cfa09033c1be5a15edcf85ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b18dd2f5a2d2133fdefdd53b52cadd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0c4c5859496222c3592",
+    "url": "/robofriends/static/css/main.8f66eaee.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c4c5859496222c3592",
+    "url": "/robofriends/static/js/main.0f3904ff.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
